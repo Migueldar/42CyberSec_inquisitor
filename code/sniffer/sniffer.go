4b928c082d77363e8a2264a2c80e27713861894e
Daniel Rodriguez pueyo <danrodri@c1r17s2.42madrid.com>
2022-08-11 06:01:57 +0200

    first commit; layout of pcap-based sniffer

---
+++ code/sniffer/sniffer.go
@@ -0,0 +1,68 @@
+package main
+/* package sniffer */
+
+import (
+	"fmt"
+	"log"
+	"errors"
+//	"github.com/google/gopacket"
+    "github.com/google/gopacket/pcap"
+)
+
+/* 
+** Loops through array of available interfaces on host,
+** then chooses first one that is not loopback (lo0)
+*/ 
+func selectInterface() (pcap.Interface, error) {
+	ifaces, err := pcap.FindAllDevs()
+	if err != nil {
+		return pcap.Interface{}, err
+	}
+	for _, iface := range ifaces {
+		if iface.Name != "lo0" && iface.Name != "lo" {
+			return iface, nil
+		}
+	}
+	return pcap.Interface{}, errors.New("could not find valid network interface")
+}
+
+func main() /*Sniffer*/ {
+	/* provide interface name & subnet mask */
+	iface, err := selectInterface()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dev  := iface.Name
+	/* nmsk := iface.Addresses[0].Netmask */
+
+	/* 
+	** Open provided device for sniffing (open session vaya)
+	** func OpenLive(device string, snaplen int32, promisc bool, timeout time.Duration) (handle *Handle, _ error) 
+	**		device:  device for which pcap will listen
+	** 		snaplen: max. size to read for packet
+	**		promisc: set promiscuous mode on/off
+	**		timeout: tal
+	*/
+	handle, err := pcap.OpenLive(dev, 1024, false, pcap.BlockForever)
+	if err != nil {
+		fmt.Println(err)
+		/* return err */
+	}
+	defer handle.Close()
+	/*
+	** filter traffic: compile and apply (netmask needed)
+	** func (p *Handle) CompileBPFFilter(expr string) ([]BPFInstruction, error)
+	** 		expr:   BPF filter expresion (man 7 pcap-filter)
+	**		return: Compiled BPF instruction
+	*/
+	BPFins, err := handle.CompileBPFFilter("tcp and port 80")
+	if err != nil {
+		fmt.Println(err)
+		/* return err */
+	}
+	fmt.Println(BPFins)
+
+	/* set filter */
+
+	/* pcap_loop */
+}
