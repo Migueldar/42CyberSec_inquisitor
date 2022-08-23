d403b984278192901f99f9072cc98cfad1e1db56
Daniel Rodriguez pueyo <danrodri@c1r16s1.42madrid.com>
2022-08-23 16:38:05 +0200

    cleanup

--- code/setup.sh
+++
@@ -1,7 +0,0 @@
-docker run \
--it \
---rm \
---name test \
--v $(pwd)/sniffer:/app/ \
-debian:bullseye-slim \
-bash
--- code/sniffer/docker-setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cd /app/ && apt update && apt install -y libpcap-dev golang git ca-certificates && go mod init test && go get .
